6edd0251300ecea431f708c43667fa1fc1fc3adc
AnastasiosParaskevopoulos <aparaskevdev@gmail.com>
2025-10-11 01:27:21 +0300

    Basic structure of project Workstream

---
+++ src/test/java/paraskevopoulos/workstream/WorkstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package paraskevopoulos.workstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
